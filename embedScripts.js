667fb41cff35b4233618b1334a1d7fe34c18740c
Santtu Pesonen <55315235+MFG38@users.noreply.github.com>
2021-07-29 19:06:43 +0300

    Another experiment.

---
+++ embedScripts.js
@@ -0,0 +1,7 @@
+document.getElementById("#navbarPlaceholder").innerHTML =
+'<nav class="navbar">' +
+'	<a href="../index.html">Home</a>' +
+'	<a href="../doom_wad_index.html">Doom WADs</a>' +
+'	<a href="../game_index.html">Games</a>' +
+'	<a href="../tutorial_index.html">Tutorials</a>' +
+'</nav>';
