3de79ca1ae1a1c81ea6f8368abd6a01de436562e
Santtu Pesonen <55315235+MFG38@users.noreply.github.com>
2021-07-29 20:34:23 +0300

    Revert "So now if I do this the way I initially intended..."
    This reverts commit 4db8db1424c14fe613e0690aee2a667985e33e8f.

--- embedScripts.js
+++ embedScripts.js
@@ -1,7 +1,9 @@
-document.getElementById('navbarPlaceholder').innerHTML =
-	'<nav class="navbar">' +
-	'	<a href="index.html">Home</a>' +
-	'	<a href="doom_wad_index.html">Doom WADs</a>' +
-	'	<a href="game_index.html">Games</a>' +
-	'	<a href="tutorial_index.html">Tutorials</a>' +
-	'</nav>';
+function EmbedNavbar () {
+	document.getElementById('navbarPlaceholder').innerHTML =
+		'<nav class="navbar">' +
+		'	<a href="index.html">Home</a>' +
+		'	<a href="doom_wad_index.html">Doom WADs</a>' +
+		'	<a href="game_index.html">Games</a>' +
+		'	<a href="tutorial_index.html">Tutorials</a>' +
+		'</nav>';
+}
