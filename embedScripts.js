8b37467efbc57d0a4488c3044ac539a91305a0ec
Santtu Pesonen <55315235+MFG38@users.noreply.github.com>
2021-07-29 20:47:40 +0300

    And then do the big updating shenanigans.

--- embedScripts.js
+++ embedScripts.js
@@ -1,9 +1,9 @@
 function EmbedNavbar () {
 	document.getElementById('navbarPlaceholder').innerHTML =
 		'<nav class="navbar">' +
-		'	<a href="index.html">Home</a>' +
-		'	<a href="doom_wad_index.html">Doom WADs</a>' +
-		'	<a href="game_index.html">Games</a>' +
-		'	<a href="tutorial_index.html">Tutorials</a>' +
+		'	<a href="../index.html">Home</a>' +
+		'	<a href="../doom_wad_index.html">Doom WADs</a>' +
+		'	<a href="../game_index.html">Games</a>' +
+		'	<a href="../tutorial_index.html">Tutorials</a>' +
 		'</nav>';
 }
