cddbb2d203c1a1f7f7737cf4e35f08e3b2d4f460
Santtu Pesonen <55315235+MFG38@users.noreply.github.com>
2021-07-29 19:43:33 +0300

    Update embedScripts.js

--- embedScripts.js
+++ embedScripts.js
@@ -3,8 +3,8 @@ var navbarHTML = navbar.innerHTML;
 
 navbar.innerHTML =
 	'<nav class="navbar">' +
-	'	<a href="../index.html">Home</a>' +
-	'	<a href="../doom_wad_index.html">Doom WADs</a>' +
-	'	<a href="../game_index.html">Games</a>' +
-	'	<a href="../tutorial_index.html">Tutorials</a>' +
+	'	<a href="index.html">Home</a>' +
+	'	<a href="doom_wad_index.html">Doom WADs</a>' +
+	'	<a href="game_index.html">Games</a>' +
+	'	<a href="tutorial_index.html">Tutorials</a>' +
 	'</nav>';
