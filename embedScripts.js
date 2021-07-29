3c2a0026b8e23057b178cfaf02c649e550cabb88
Santtu Pesonen <55315235+MFG38@users.noreply.github.com>
2021-07-29 20:04:18 +0300

    Maybe now?

--- embedScripts.js
+++ embedScripts.js
@@ -1,10 +1,11 @@
-var navbar = document.querySelector('#navbarPlaceholder');
-var navbarHTML = navbar.innerHTML;
+function EmbedNavbar () {
+	var navbar = document.getElementById('#navbarPlaceholder');
 
-navbar.innerHTML =
-	'<nav class="navbar">' +
-	'	<a href="index.html">Home</a>' +
-	'	<a href="doom_wad_index.html">Doom WADs</a>' +
-	'	<a href="game_index.html">Games</a>' +
-	'	<a href="tutorial_index.html">Tutorials</a>' +
-	'</nav>';
+	navbar.innerHTML =
+		'<nav class="navbar">' +
+		'	<a href="index.html">Home</a>' +
+		'	<a href="doom_wad_index.html">Doom WADs</a>' +
+		'	<a href="game_index.html">Games</a>' +
+		'	<a href="tutorial_index.html">Tutorials</a>' +
+		'</nav>';
+}
