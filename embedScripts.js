4db8db1424c14fe613e0690aee2a667985e33e8f
Santtu Pesonen <55315235+MFG38@users.noreply.github.com>
2021-07-29 20:25:06 +0300

    So now if I do this the way I initially intended...

--- embedScripts.js
+++ embedScripts.js
@@ -1,9 +1,7 @@
-function EmbedNavbar () {
-	document.getElementById('navbarPlaceholder').innerHTML =
-		'<nav class="navbar">' +
-		'	<a href="index.html">Home</a>' +
-		'	<a href="doom_wad_index.html">Doom WADs</a>' +
-		'	<a href="game_index.html">Games</a>' +
-		'	<a href="tutorial_index.html">Tutorials</a>' +
-		'</nav>';
-}
+document.getElementById('navbarPlaceholder').innerHTML =
+	'<nav class="navbar">' +
+	'	<a href="index.html">Home</a>' +
+	'	<a href="doom_wad_index.html">Doom WADs</a>' +
+	'	<a href="game_index.html">Games</a>' +
+	'	<a href="tutorial_index.html">Tutorials</a>' +
+	'</nav>';
