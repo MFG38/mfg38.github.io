caf275963055fab9e17e03ff039bfe64c8ddfdac
MFG38 <santtu.pesonen@mail.com>
2021-10-11 11:31:32 +0300

    Add MIDIs

--- embedScripts.js
+++ embedScripts.js
@@ -4,7 +4,7 @@ function EmbedNavbar () {
 		'	<a href="../index.html">Home</a>' +
 		'	<a href="../doom_wad_index.html">Doom WADs</a>' +
 		'	<a href="../game_index.html">Games</a>' +
-		'	<a href="../software_index.html">Software</a>' +
+		'	<a href="../midi_index.html">MIDIs</a>' +
 		'	<a href="../tutorial_index.html">Tutorials</a>' +
 		'</nav>';
 }
