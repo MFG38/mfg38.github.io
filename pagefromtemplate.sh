051e26db6f6ff0eb46cbf7e38c446a0ffa1752bb
MFG38 <santtu.pesonen@mail.com>
2024-07-12 09:33:03 +0300

    Improve pagefromtemplate.sh

--- pagefromtemplate.sh
+++ pagefromtemplate.sh
@@ -1,14 +1,23 @@
 #!/bin/env bash
 
-case $1 in
-	doom)
-		cp ./templates/doom_wad.html ./doom/$2.html
-		;;
-	game)
-		cp ./templates/game.html ./games/$2.html
-		;;
-	tutorial)
-		cp ./templates/tutorial.html ./tutorials/$2.html
-		;;
-esac
+if [ $# -lt 2 ]; then
+	echo "Not enough arguments - 2 expected."
+	echo "Arg1 = template name (doom, game or tutorial)"
+	echo "Arg2 = filename of generated page"
+else
+	case $1 in
+		doom)
+			echo "Creating page doom/$2.html..."
+			cp ./templates/doom_wad.html ./doom/$2.html
+			;;
+		game)
+			echo "Creating page game/$2.html..."
+			cp ./templates/game.html ./games/$2.html
+			;;
+		tutorial)
+			echo "Creating page tutorial/$2.html..."
+			cp ./templates/tutorial.html ./tutorials/$2.html
+			;;
+	esac
+fi
 
